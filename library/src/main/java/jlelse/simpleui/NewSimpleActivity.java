ba9c1f4131d5ca0dd7d09dbb918c9096b9125781
Jan-Lukas Else <janlukas.else@gmail.com>
2015-10-31 20:56:17 +0100

    Some more changes Still not finish

--- library/src/main/java/jlelse/simpleui/NewSimpleActivity.java
+++ library/src/main/java/jlelse/simpleui/NewSimpleActivity.java
@@ -20,9 +20,17 @@ import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * New try to implement the SimpleActivity better. Will be renamed to SimpleActivity later and will replace old one.
@@ -30,23 +38,22 @@ import android.view.View;
 
 public class NewSimpleActivity extends AppCompatActivity implements View.OnClickListener {
 
-
     //Fab
     private boolean fabEnabled = false;
     private Drawable fabDrawable = null;
     private int fabColor = getResources().getColor(R.color.default_accent);
     private FloatingActionButton floatingActionButton;
 
-
     //Toolbar
     private boolean toolbarEnabled = true;
     private int toolbarColor = getResources().getColor(R.color.default_toolbar);
     private Toolbar toolbar;
 
-    //Colors
-    private int primaryColor = getResources().getColor(R.color.default_primary);
-    private int primaryDarkColor = getResources().getColor(R.color.default_primary_dark);
-    private int accentColor = getResources().getColor(R.color.default_accent);
+    //Drawer
+    private DrawerLayout drawerLayout = null;
+
+    //Other
+    private LinearLayout mainLayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,20 +61,39 @@ public class NewSimpleActivity extends AppCompatActivity implements View.OnClick
 
         enableDefaultTheme();
 
-        setContentView(R.layout.main);
+        // Important because own methods override it
+        super.setContentView(R.layout.main);
 
+        initViews();
+    }
+
+    private void initViews() {
         floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mainLayout = (LinearLayout) findViewById(R.id.main);
+
+        setSupportActionBar(toolbar);
+    }
+
+    private void changeLayout(int resId) {
+        List<View> views = new ArrayList<>();
+        for (int i = 0; i < mainLayout.getChildCount(); i++) {
+            views.add(mainLayout.getChildAt(i));
+        }
+        super.setContentView(resId);
+        initViews();
+        for (View view : views) {
+            mainLayout.addView(view);
+        }
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            default:
+                break;
+        }
     }
-
-
-    // Other methods
 
     //Fab
 
@@ -136,7 +162,6 @@ public class NewSimpleActivity extends AppCompatActivity implements View.OnClick
         return toolbar;
     }
 
-
     //Drawer
 
     /**
@@ -144,10 +169,35 @@ public class NewSimpleActivity extends AppCompatActivity implements View.OnClick
      */
     public void enableDrawer(boolean enabled) {
         if (enabled) {
+            changeLayout(R.layout.with_drawer);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+            drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
+                    invalidateOptionsMenu();
+                    syncState();
+                }
 
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    super.onDrawerClosed(drawerView);
+                    invalidateOptionsMenu();
+                    syncState();
+                }
+            };
+            drawerLayout.setDrawerListener(actionBarDrawerToggle);
+            actionBarDrawerToggle.syncState();
         } else {
-
+            changeLayout(R.layout.main);
+            drawerLayout = null;
         }
+    }
+
+    public DrawerLayout getDrawerLayout() {
+        return drawerLayout;
     }
 
     //Theme
@@ -160,7 +210,8 @@ public class NewSimpleActivity extends AppCompatActivity implements View.OnClick
     }
 
     /**
-     * Enables a custom theme
+     * Enables a custom theme and applies possibility to use own colors etc.
+     * Please use as theme parent the themes provided by this library!
      *
      * @param resId ID of style resource
      */
@@ -168,50 +219,41 @@ public class NewSimpleActivity extends AppCompatActivity implements View.OnClick
         setTheme(resId);
     }
 
-    // Colors
+    // Content
 
-    /**
-     * Sets the primary color
-     *
-     * @param color       The color which should be used as primary color
-     * @param alsoToolbar If true the color will be used for the Toolbar too
-     */
-    public void setPrimaryColor(int color, boolean alsoToolbar) {
-        primaryColor = color;
-        if (alsoToolbar) {
-            toolbarColor = color;
-            enableToolbar(toolbarEnabled);
+    //own method
+    private void addContent(View view, boolean override) {
+        if (override) {
+            mainLayout.removeAllViews();
         }
-        applyColorsToTheme();
+        mainLayout.addView(view);
     }
 
-    /**
-     * Sets the primary_dark color
-     *
-     * @param color The color which should be used as primary_dark color
-     */
-    public void setPrimaryDarkColor(int color) {
-        primaryDarkColor = color;
-        applyColorsToTheme();
-    }
-
-    /**
-     * Sets the accent color
-     *
-     * @param color   The color which should be used as accent color
-     * @param alsoFab If true this color will be used for the FloatingActionButton too
-     */
-    public void setAccentColor(int color, boolean alsoFab) {
-        accentColor = color;
-        if (alsoFab) {
-            fabColor = color;
-            enableFab(fabEnabled);
+    private void addContent(View view, ViewGroup.LayoutParams layoutParams, boolean override) {
+        if (override) {
+            mainLayout.removeAllViews();
         }
-        applyColorsToTheme();
+        mainLayout.addView(view, layoutParams);
     }
 
-    private void applyColorsToTheme() {
-
+    @Override
+    public void setContentView(int layoutResID) {
+        View view = getLayoutInflater().inflate(layoutResID, null);
+        addContent(view, true);
     }
 
+    @Override
+    public void setContentView(View view) {
+        addContent(view, true);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        addContent(view, params, true);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        addContent(view, params, false);
+    }
 }
